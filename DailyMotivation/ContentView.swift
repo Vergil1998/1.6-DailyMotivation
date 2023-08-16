c6c0a450e3653b1b55dab6496bfe932e4180e4d9
Daredevil <kaaan_s@yahoo.de>
2023-08-16 16:45:25 +0200

    - Chapter 1.6 of DailyMotivation

--- DailyMotivation/ContentView.swift
+++ DailyMotivation/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "Yo!"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        ZStack {
+            Rectangle()
+                .fill(Gradient(colors: [Color("SkyBlue"), .indigo]))
+                .ignoresSafeArea()
+            
+          
+            
+            VStack(spacing: 0) {
+              
+                
+                Text("You Have Skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("Gold-BC"))
+                    .padding()
+                    .background(Color("Maroon-BC"))
+                    .cornerRadius(15)
+                
+                
+                
+              Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .border(.orange, width: 1)
+                    .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You Are Awesome!"
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                //   .border(.purple, width: 5)
+                .padding()
+                .tint(Color("Vermillion"))
+                
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
